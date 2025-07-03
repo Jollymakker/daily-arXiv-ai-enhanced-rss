4dd5fc747bc1c27c9604503474efd07f19631be7
wudi <973792200@qq.com>
2025-07-03 15:08:22 +0800

    1

--- api/database.py
+++ api/database.py
@@ -9,11 +9,11 @@ class DatabaseManager:
         self.conn_string = self._get_connection_string()
 
     def _get_connection_string(self):
-        db_host = os.environ.get("DB_HOST", "ep-billowing-hill-adl8i28r-pooler.c-2.us-east-1.aws.neon.tech")
-        db_name = os.environ.get("DB_NAME", "neondb")
-        db_user = os.environ.get("DB_USER", "neondb_owner")
-        db_password = os.environ.get("DB_PASSWORD", "npg_ME1pQmiva9Tl")
-        db_port = os.environ.get("DB_PORT", "")
+        db_host = os.environ.get("DB_HOST")
+        db_name = os.environ.get("DB_NAME")
+        db_user = os.environ.get("DB_USER")
+        db_password = os.environ.get("DB_PASSWORD")
+        db_port = os.environ.get("DB_PORT")
 
         if not all([db_host, db_name, db_user, db_password]):
             self.logger.error("数据库连接环境变量未完全设置，无法建立数据库连接。")
